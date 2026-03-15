3b7904e8860d722e8efd8db4ef3e4bd6b46f148e
Jack Blaze <jackblaze236@gmail.com>
2026-03-15 13:20:46 +0500

    useLocation Hook

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,19 +1,25 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { users } from "../constants/constants";
 
 const UserDetails = () => {
-  const { id: userId } = useParams();
+  const routeParams = useParams();
+  const { id: userId } = routeParams;
+
   const user = users.find((user) => user.id === parseInt(userId));
+
+  const locationParams = useLocation();
+  console.log("location", locationParams);
 
   if (!user) return <p>Loading</p>;
 
   return (
     <div>
       <h2>Welcome</h2>
+      {locationParams.pathname.includes("/user/1") && <p>User 1 specific content</p>}
       <p>ID: {user.id}</p>
       <p>Name: {user.name}</p>
-      <p>Name: {user.email}</p>
+      <p>Email: {user.email}</p>
     </div>
   );
 };
