38ad81426389522279c9c722d24c36a3d64745f8
Jack Blaze <jackblaze236@gmail.com>
2026-03-15 13:41:44 +0500

    Dynamic UI based on routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,26 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navLinkStyles = ({ isActive }) => ({
     backgroundColor: isActive ? "black" : "transparent",
     color: isActive ? "white" : "black",
+    padding: "4px 8px",
+    borderRadius: "4px"
   });
+
+  const navigate = useNavigate();
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const login = () => {
+    setLoggedIn(true);
+    navigate("/users");
+  };
+
+  const logout = () => {
+    setLoggedIn(false);
+    navigate("/");
+  };
 
   return (
     <>
@@ -18,18 +33,44 @@ const Navbar = () => {
           backgroundColor: "white",
         }}
       >
-        <NavLink to={"/"} style={navLinkStyles}>
-          Home
-        </NavLink>
-        <NavLink to={"/about"} style={navLinkStyles}>
-          About
-        </NavLink>
-        <NavLink to={"/contact"} style={navLinkStyles}>
-          Contact us
-        </NavLink>
-        <NavLink to={"/users"} style={navLinkStyles}>
-          User
-        </NavLink>
+        {isLoggedIn && (
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <NavLink to={"/users"} style={navLinkStyles}>
+              Users
+            </NavLink>
+            <button onClick={logout}>Logout</button>
+          </div>
+        )}
+        {!isLoggedIn && (
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <div style={{display: "flex", gap: "12px"}}>
+              <NavLink to={"/"} style={navLinkStyles}>
+                Home
+              </NavLink>
+              <NavLink to={"/about"} style={navLinkStyles}>
+                About
+              </NavLink>
+              <NavLink to={"/contact"} style={navLinkStyles}>
+                Contact us
+              </NavLink>
+            </div>
+            <button onClick={login}>Login</button>
+          </div>
+        )}
       </div>
     </>
   );
--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useParams, useNavigate } from "react-router-dom";
 import { users } from "../constants/constants";
 
 const UserDetails = () => {
+  const navigate = useNavigate();
   const routeParams = useParams();
   const { id: userId } = routeParams;
 
@@ -16,10 +17,24 @@ const UserDetails = () => {
   return (
     <div>
       <h2>Welcome</h2>
-      {locationParams.pathname.includes("/user/1") && <p>User 1 specific content</p>}
+      {locationParams.pathname.includes("/user/1") && (
+        <p>User 1 specific content</p>
+      )}
       <p>ID: {user.id}</p>
       <p>Name: {user.name}</p>
       <p>Email: {user.email}</p>
+
+      {/* <div style={{ display: "flex", gap: "12px" }}>
+        <button style={{ marginTop: "12px" }} onClick={() => navigate("/about")}>
+          About
+        </button>
+        <button style={{ marginTop: "12px" }} onClick={() => navigate("/contact")}>
+          Contact
+        </button>
+        <button style={{ marginTop: "12px" }} onClick={() => navigate("/")}>
+          Logout
+        </button>
+      </div> */}
     </div>
   );
 };
